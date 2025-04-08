ea8fad2ac453c110990285535c4a6b5c2afd5262
khush-stardust <khushimadikar2005@gmail.com>
2025-04-08 12:12:31 +0530

    new file div

---
+++ div.java
@@ -0,0 +1,10 @@
+package JAVA;
+
+public class div {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int quotient = a / b;
+        System.out.println("The quotient is: " + quotient);
+    }
+}
