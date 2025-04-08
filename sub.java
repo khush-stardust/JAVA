ddcdf69f419c9262c20dc41074b009af36fcbb37
khush-stardust <khushimadikar2005@gmail.com>
2025-04-08 11:58:27 +0530

    new file sub

---
+++ sub.java
@@ -0,0 +1,11 @@
+package JAVA;
+
+public class sub {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int difference = a - b;
+        System.out.println("The difference is: " + difference);
+    }
+    
+}
